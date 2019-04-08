1b3c6b52fcbeafa162f616be0c52d23ba5f4d4c3
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-04-08 16:29:31 -0400

    Updated perplexity tests for HRED

--- perplexity_study.py
+++ perplexity_study.py
@@ -3,13 +3,10 @@ import getopt
 import math
 import matplotlib.pyplot as plt
 from pathlib import Path
-import os
-import random
-import re
-import sys
+import os, pickle, random, re, sys
 
-import story_generation as storygen
-import storygen.seq2seq as seq2seq
+import hred_story_generation as storygen
+from storygen.hred import Hred
 import storygen.log as log
 
 OBJ_DIR = 'obj'
@@ -23,7 +20,7 @@ HELP_MSG = ''.join([
 ])
 
 
-def calculate_perplexities(network, words, pairs):
+def calculate_perplexities(network, words, paragraphs, sentences, sentences_by_lengths):
      # Actual book sentences
     actual_sentences_score = 0.0
 
@@ -33,48 +30,59 @@ def calculate_perplexities(network, words, pairs):
     # Random sentences
     random_sentences_score = 0.0
     
+    # Exact random sentences
+    exact_random_sentences_score = 0.0
+
     # Calculate scores
     i = 0
     percentages = {0}
-    curr_len = len(pairs)
-    for pair in pairs:
+    curr_len = len(paragraphs)
+    for paragraph in paragraphs:
         # Calculate the actual perplexity
-        actual_perplexity = network._evaluate_specified(pair[0], pair[1])
+        actual_perplexity = network._evaluate_specified(paragraph, paragraph[-1])
 
         # Calculate the perplexity of sentences (of the same length) build of random words in the vocab 
-        random_words = ' '.join([random.choice(words) for i in range(len(pair[1].split()))])
-        words_perplexity = network._evaluate_specified(pair[0], random_words)
+        random_words = [random.choice(words) for i in range(len(paragraph[-1]))]
+        words_perplexity = network._evaluate_specified(paragraph, random_words)
         
         # Calculate the perplexity of sentences taken at random from the pairs
-        random_sentence = random.choice(random.choice(pairs))
-        sentence_perplexity = network._evaluate_specified(pair[0], random_sentence)
+        random_sentence = random.choice(sentences)
+        sentence_perplexity = network._evaluate_specified(paragraph, random_sentence)
 
-        if actual_perplexity is not None and words_perplexity is not None and sentence_perplexity is not None:
+        # Calculate the perplexity of sentences taken at random of the same length as target sentence
+        # TODO: pre-calculate a dictionary for all sentence lengths
+        exact_random_sentence = random.choice(sentences_by_lengths[len(paragraph[-1])])
+        exact_sentence_perplexity = network._evaluate_specified(paragraph, exact_random_sentence)
+
+        if actual_perplexity is not None and words_perplexity is not None and sentence_perplexity is not None and exact_sentence_perplexity is not None:
             actual_sentences_score += actual_perplexity
             random_words_score += words_perplexity
             random_sentences_score += sentence_perplexity
+            exact_random_sentences_score += exact_sentence_perplexity
         else:
             curr_len -= 1
             if actual_perplexity is None:
-                print('(i={}) Actual: retrieved a 0-d tensor from ([{}], [{}])'.format(i, pair[0], pair[1]))
+                print(f'(i={i}) Actual: retrieved a 0-d tensor from ({paragraph})')
             if words_perplexity is None:
-                print('(i={}) Random words: retrieved a 0-d tensor from ([{}], [{}])'.format(i, pair[0], random_words))
+                print(f'(i={i}) Random words: retrieved a 0-d tensor from ([{paragraph}], [{random_words}])')
             if sentence_perplexity is None:
-                print('(i={}) Random sentences: retrieved a 0-d tensor from ([{}], [{}])'.format(i, pair[0], random_sentence))
+                print(f'(i={i}) Random sentences: retrieved a 0-d tensor from ([{paragraph}], [{random_sentence}])')
+            if exact_sentence_perplexity is None:
+                print(f'(i={i}) Exact random sentences: retrieved a 0-d tensor from ([{paragraph}], [{exact_random_sentence}])')
 
         i += 1
-        percentage = math.floor(100 * (float(i) / len(pairs)))
+        percentage = math.floor(100 * (float(i) / len(paragraphs)))
         if percentage % 5 == 0 and percentage not in percentages:
             print('{}% complete.'.format(percentage))
             percentages.add(percentage)
 
-    return actual_sentences_score / curr_len, random_words_score / curr_len, random_sentences_score / curr_len
+    return actual_sentences_score / curr_len, random_words_score / curr_len, random_sentences_score / curr_len, exact_random_sentences_score / curr_len
 
 # Study on the perplexity module using the provided text
 def main(argv):
     # Get command line arguments
     try:
-        opts, _ = getopt.getopt(argv, 'h', ['embedding=', 'build=', 'help'])
+        opts, _ = getopt.getopt(argv, 'hf:', ['embedding=', 'build=', 'help', 'file='])
     except getopt.GetoptError as e:
         print(e)
         print(HELP_MSG)
@@ -86,12 +94,15 @@ def main(argv):
 
     # Default value
     embedding_type = None
+    checkpoint_filename = None
 
     # Set values from command line
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             print(HELP_MSG)
             exit()
+        if opt in ('-f', '--file'):
+            checkpoint_filename = arg
         elif opt == '--embedding':
             embedding_type = arg
             if embedding_type not in EMBEDDING_TYPES:
@@ -131,129 +142,131 @@ def main(argv):
     else:
         obj_dir += '/'
 
-    # Title of book that was trained on
-    book_title = '1_sorcerers_stone'
+    # prepare data
+    train_paragraphs, validation_paragraphs, test_paragraphs = [], [], []
+    with open('data/train_raw.pkl', 'rb') as f:
+        train_paragraphs = pickle.load(f)
+    with open('data/validate_raw.pkl', 'rb') as f:
+        validation_paragraphs = pickle.load(f)
+    with open('data/test_raw.pkl', 'rb') as f:
+        test_paragraphs = pickle.load(f)
 
-    # Get train and test pairs
-    train_pairs, test_pairs = storygen.get_pairs(book_title, 0.8, pre_parsed=False)
-        
-	# Set MAX_LENGTH:	
+    paragraphs = train_paragraphs + validation_paragraphs + test_paragraphs
+
+    # Create a list of all sentences
+    test_sentences = [sentence for paragraph in test_paragraphs for sentence in paragraph]
+    test_sentences_by_lengths = {}
+    for sentence in test_sentences:
+        if len(sentence) in test_sentences_by_lengths:
+            test_sentences_by_lengths[len(sentence)].append(sentence)
+        else:
+            test_sentences_by_lengths[len(sentence)] = [sentence]
+
     MAX_LENGTH = max(
-        max(map(len, [sentence.split() for pair in train_pairs for sentence in pair])),
-        max(map(len, [sentence.split() for pair in test_pairs for sentence in pair])))
+        max(map(len, [sentence for sentence in paragraph]))
+    for paragraph in paragraphs)
     MAX_LENGTH += 1 # for <EOL> token
 
-    # Create book of vocab indexes
-    book = storygen.get_book(book_title, train_pairs, test_pairs)
+    book_title = '1_sorcerers_stone'
 
-    # Find the largest checkpoint (code lifted from seq2seq.train_iters)
-    encoders = set()
-    decoders = set()
-    for filename in os.listdir(obj_dir):
-        r_enc = re.search(seq2seq.CHECKPOINT_FORMAT.format('encoder', storygen.EMBEDDING_SIZE, storygen.HIDDEN_SIZE, MAX_LENGTH), filename)
-        if r_enc:
-            encoders.add(int(r_enc.group(1)))
-        else:
-            r_dec = re.search(seq2seq.CHECKPOINT_FORMAT.format('decoder', storygen.EMBEDDING_SIZE, storygen.HIDDEN_SIZE, MAX_LENGTH), filename)
-            if r_dec:
-                decoders.add(int(r_dec.group(1)))
-
-    # A checkpoint needs a valid encoder and decoder 
-    checkpoints = encoders.intersection(decoders)
+    # Create a book object from the train/test pairs
+    book = storygen.get_book(book_title, paragraphs)    
     
-    if len(checkpoints) > 0:
-        print('Checkpoints found at: {}'.format(checkpoints))
-        logger.debug(logfile, 'Checkpoints found at: {}'.format(checkpoints))
-    else:
-        print('No checkpoints found for {}'.format(embedding_type))
-        logger.error(logfile, 'No checkpoints found for {}'.format(embedding_type))
+    # Create and load the model
+    hred = Hred(
+        hidden_size=storygen.HIDDEN_SIZE,
+        max_length=MAX_LENGTH,
+        embedding_size=storygen.EMBEDDING_SIZE,
+        optimizer_type='sgd', # Currently hard-coded for SGD optimizers
+        book=book,
+        use_cuda=storygen.USE_CUDA
+    )
+    if not hred.loadFromFiles(checkpoint_filename):
+        print(f'Error loading checkpoint at {checkpoint_filename}')
+        logger.error(logfile, f'Error loading checkpoint at {checkpoint_filename}')
         exit(1)
 
-    epoch_size = max(checkpoints)
-    print('Epoch size used for study: {}'.format(epoch_size))
-    logger.info(logfile, 'Epoch size used for study: {}'.format(epoch_size))
-
-    # Get encoder/decoder file formats
-    encoder_filename = seq2seq.ENCODER_FILE_FORMAT.format(obj_dir, epoch_size, storygen.EMBEDDING_SIZE, storygen.HIDDEN_SIZE, MAX_LENGTH)
-    decoder_filename = seq2seq.DECODER_FILE_FORMAT.format(obj_dir, epoch_size, storygen.EMBEDDING_SIZE, storygen.HIDDEN_SIZE, MAX_LENGTH)
-
-    network = seq2seq.Seq2Seq(book, MAX_LENGTH, storygen.HIDDEN_SIZE, storygen.EMBEDDING_SIZE, storygen.DEVICE)
-    if not network.loadFromFiles(encoder_filename, decoder_filename):
-        print('Error loading encoder at {}\nError loading decoder at {}'.format(encoder_filename, decoder_filename))
-        logger.error(logfile, 'Error loading encoder at {}\nError loading decoder at {}'.format(encoder_filename, decoder_filename))
-        exit(1)
-
-    # Actual book sentences
-    actual_sentences_score_train = 0.0
-    actual_sentences_score_test = 0.0
-
-    # Random words
-    random_words_score_train = 0.0
-    random_words_score_test = 0.0
+    EPOCHS = 100
     words = list(book.word2index)
-    
-    # Random sentences
-    random_sentences_score_train = 0.0
-    random_sentences_score_test = 0.0
-    
-    # Calculate scores on training data
-    print('Calculating scores on training data...')
 
-    actual_sentences_score_train, random_words_score_train, random_sentences_score_train = calculate_perplexities(network, words, train_pairs)
+    # Final scores
+    final_actual_sentences_score_test = 0.0
+    final_random_words_score_test = 0.0
+    final_random_sentences_score_test = 0.0
+    final_exact_random_sentences_score_test = 0.0
 
-    print('Calculating scores on test data...')
-    actual_sentences_score_test, random_words_score_test, random_sentences_score_test = calculate_perplexities(network, words, test_pairs)
-    
-    print('Actual sentences score (train): {:.4f}'.format(actual_sentences_score_train))
-    print('Actual sentences score (test): {:.4f}'.format(actual_sentences_score_test))
-    logger.info(logfile, 'Actual sentences score (train): {:.4f}'.format(actual_sentences_score_train))
-    logger.info(logfile, 'Actual sentences score (test): {:.4f}'.format(actual_sentences_score_test))
+    for i in range(EPOCHS):
+        print(f'Processing epoch {i+1}...')
+        # Actual book sentences
+        #actual_sentences_score_train = 0.0
+        actual_sentences_score_test = 0.0
 
-    print('Random words score (train): {:.4f}'.format(random_words_score_train))
-    print('Random words score (test): {:.4f}'.format(random_words_score_test))
-    logger.info(logfile, 'Random words score (train): {:.4f}'.format(random_words_score_train))
-    logger.info(logfile, 'Random words score (test): {:.4f}'.format(random_words_score_test))
+        # Random words
+        #random_words_score_train = 0.0
+        random_words_score_test = 0.0
+        #words = list(book.word2index)
+        
+        # Random sentences
+        #random_sentences_score_train = 0.0
+        random_sentences_score_test = 0.0
 
-    print('Random sentences score (train): {:.4f}'.format(random_sentences_score_train))
-    print('Random sentences score (test): {:.4f}'.format(random_sentences_score_test))
-    logger.info(logfile, 'Random sentences score (train): {:.4f}'.format(random_sentences_score_train))
-    logger.info(logfile, 'Random sentences score (test): {:.4f}'.format(random_sentences_score_test))
+        # Excat random sentences
+        #exact_random_sentences_score_train = 0.0
+        exact_random_sentences_score_test = 0.0
+        
+        # Calculate scores on training data
+        #print('Calculating scores on training data...')
 
-    """ Temporarily removed due to index out-of-bounds errors...
-    # Sentence segments (n-grams)
-    sentences = storygen.get_sentences(book_title)
-    n_gram_lengths = [1,2,3,4,6,8,12]
-    for n_gram in n_gram_lengths:
-        n_grams = []
-        print('Processing n_gram: {}'.format(n_gram))
-        for sentence in sentences:
-            words = sentence.split()
-            if len(words) >= n_gram:
-                n_grams += [words[i:i+n_gram] for i in range(len(words)-n_gram+1)]
+        #actual_sentences_score_train, random_words_score_train, random_sentences_score_train, exact_random_sentences_score_train = calculate_perplexities(hred, words, train_paragraphs)
 
-        n_gram_score = 0.0
-        incorrect_lengths = 0
-        i = 0
-        percentages = {0}
-        for sentence in sentences:
-            n_gram_sentence = sentence.split()
-            if len(n_gram_sentence) >= n_gram:
-                for i in range(int(len(n_gram_sentence)/n_gram)):
-                    n_gram_sentence[n_gram*i:n_gram*(i+1)] = random.choice(n_grams)
-                    perplexity = network._evaluate_specified(sentence, ' '.join(n_gram_sentence))
-                    n_gram_score += perplexity
-            else:
-                incorrect_lengths += 1
+        #print('Calculating scores on test data...')
+        actual_sentences_score_test, random_words_score_test, random_sentences_score_test, exact_random_sentences_score_test = calculate_perplexities(hred, words, test_paragraphs, test_sentences, test_sentences_by_lengths)
+        
+        logger.info(logfile, f'Processing epoch {i+1}')
 
-            i += 1
-            percentage = math.floor(100 * (float(i) / len(sentences)))
-            if percentage % 5 == 0 and percentage not in percentages:
-                print('{}% complete.'.format(percentage))
-                percentages.add(percentage)
-        n_gram_score /= (len(sentences) - incorrect_lengths)
-        print('{}-gram score: {:.4f}\n{:.2f%}% of sentences not included.'.format(n_gram, n_gram_score, float(incorrect_lengths)/len(sentences) * 100))
-        logger.info(logfile, '{}-gram score: {:.4f}\n{:.2f%}% of sentences not included.'.format(n_gram, n_gram_score, float(incorrect_lengths)/len(sentences) * 100))
-    """
+        #print('Actual sentences score (train): {:.4f}'.format(actual_sentences_score_train))
+        #print('Actual sentences score (test): {:.4f}'.format(actual_sentences_score_test))
+        #logger.info(logfile, 'Actual sentences score (train): {:.4f}'.format(actual_sentences_score_train))
+        logger.info(logfile, 'Actual sentences score (test): {:.4f}'.format(actual_sentences_score_test))
+
+        #print('Random words score (train): {:.4f}'.format(random_words_score_train))
+        #print('Random words score (test): {:.4f}'.format(random_words_score_test))
+        #logger.info(logfile, 'Random words score (train): {:.4f}'.format(random_words_score_train))
+        logger.info(logfile, 'Random words score (test): {:.4f}'.format(random_words_score_test))
+
+        #print('Random sentences score (train): {:.4f}'.format(random_sentences_score_train))
+        #print('Random sentences score (test): {:.4f}'.format(random_sentences_score_test))
+        #logger.info(logfile, 'Random sentences score (train): {:.4f}'.format(random_sentences_score_train))
+        logger.info(logfile, 'Random sentences score (test): {:.4f}'.format(random_sentences_score_test))
+
+        #print('Exact random sentences score (train): {:.4f}'.format(exact_random_sentences_score_train))
+        #print('Exact random sentences score (test): {:.4f}'.format(exact_random_sentences_score_test))
+        #logger.info(logfile, 'Exact random sentences score (train): {:.4f}'.format(exact_random_sentences_score_train))
+        logger.info(logfile, 'Exact random sentences score (test): {:.4f}'.format(exact_random_sentences_score_test))
+
+        final_actual_sentences_score_test += actual_sentences_score_test
+        final_random_words_score_test += random_words_score_test
+        final_random_sentences_score_test += random_sentences_score_test
+        final_exact_random_sentences_score_test += exact_random_sentences_score_test
+
+    final_actual_sentences_score_test /= float(EPOCHS)
+    final_random_words_score_test /= float(EPOCHS)
+    final_random_sentences_score_test /= float(EPOCHS)
+    final_exact_random_sentences_score_test /= float(EPOCHS)
+
+    logger.info(logfile, 'Final perplexity scores:')
+
+    print('Actual sentences score (test): {:.4f}'.format(final_actual_sentences_score_test))
+    logger.info(logfile, 'Actual sentences score (test): {:.4f}'.format(final_actual_sentences_score_test))
+
+    print('Random words score (test): {:.4f}'.format(final_random_words_score_test))
+    logger.info(logfile, 'Random words score (test): {:.4f}'.format(final_random_words_score_test))
+
+    print('Random sentences score (test): {:.4f}'.format(final_random_sentences_score_test))
+    logger.info(logfile, 'Random sentences score (test): {:.4f}'.format(final_random_sentences_score_test))
+
+    print('Exact random sentences score (test): {:.4f}'.format(final_exact_random_sentences_score_test))
+    logger.info(logfile, 'Exact random sentences score (test): {:.4f}'.format(final_exact_random_sentences_score_test))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
